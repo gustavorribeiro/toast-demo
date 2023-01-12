df11da10d485a0503e51738d1aa0c7dbb25bcd86
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-01-12 13:30:07 -0300

    finishing basic layout/functionalities

--- app/src/main/java/com/dev/myappcompany/toastdemo/MainActivity.java
+++ app/src/main/java/com/dev/myappcompany/toastdemo/MainActivity.java
@@ -3,8 +3,17 @@ package com.dev.myappcompany.toastdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClicked(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        Toast.makeText(this, "Hello " + editText.getText().toString(), Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
